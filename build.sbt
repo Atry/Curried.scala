014e58f9adc777ae69d5a4057bed88063b423bd6
Yang Bo <atryyang@thoughtworks.com>
2019-08-11 10:24:50 -0700

    Initial implementation

---
+++ build.sbt
@@ -0,0 +1,44 @@
+enablePlugins(Example)
+
+ThisBuild / organization := "com.yang-bo"
+
+import meta._
+
+examplePackageRef := q"scala"
+
+name := "curried"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.8" % Test
+
+libraryDependencies += "org.scala-lang" % "scala-reflect" % scalaVersion.value
+
+val generateCurriedWithTypeParameters = taskKey[Seq[File]]("Generate CurriedWithTypeParameters.scala file.")
+
+generateCurriedWithTypeParameters := {
+  val outputFile = (sourceManaged in Compile).value / "scala" / "CurriedWithTypeParameters.scala"
+  IO.write(
+    outputFile,
+    q"""
+      package scala {
+        import language.experimental.macros
+        private[scala] trait CurriedWithTypeParameters extends Any {
+          ..${
+            for (i <- (1 to 22)) yield q"""
+              def apply[
+                ..${
+                  for (j <- 0 until i) yield {
+                    Type.Param(Nil, Type.Name(s"A$j"), Nil, Type.Bounds(None, None), Nil, Nil)
+                  }
+                }
+              ](varargs: Any*): Any = macro Curried.Macros.apply
+            """
+          }
+        }
+      }
+    """.syntax,
+    scala.io.Codec.UTF8.charSet
+  )
+  Seq(outputFile)
+}
+
+Compile / sourceGenerators += generateCurriedWithTypeParameters.taskValue
---
+++ src/main/scala/scala/Curried.scala
@@ -0,0 +1,68 @@
+package scala
+import language.experimental.macros
+import reflect.macros.whitebox
+private[scala] object Curried {
+  final class Macros(val c: whitebox.Context) {
+    import c.universe._
+    def apply(varargs: Tree*): Tree = {
+      val q"$callee.$_[..$typeArguments](..$_)" = c.macroApplication
+      q"""${varargs.foldLeft[Tree](q"$callee.applyBegin[..$typeArguments]") { (partiallyAppliedCallee, argument) =>
+        argument match {
+          case q"$sequenceArgument: _*" =>
+            q"$sequenceArgument.foldLeft($partiallyAppliedCallee)(_.applyNext(_))"
+          case _ =>
+            q"$partiallyAppliedCallee.applyNext($argument)"
+        }
+      }}.applyEnd"""
+    }
+  }
+}
+
+/** A marker trait that enables curried varargs.
+  *
+  * Given a function call `f(a, b, c)`,
+  * when f is a subtype of [[Curried]],
+  * it should be rewritten to `f.applyBegin.applyNext(a).applyNext(b).applyNext(c).applyEnd`.
+  *
+  * Optionally, some arguments to a Curried call may be a sequence argument marked as `_*`.
+  * Given a function call `f(p1, s1: _*, p2)`,
+  *  when translating it to the curried form,
+  *  the sequence argument will becomes a `foldLeft` call.
+  *
+  * <pre>
+  * s1.foldLeft(f.applyBegin
+  *   .applyNext(p1)
+  * )(_.applyNext(_))
+  *   .applyNext(p2)
+  * .applyEnd
+  * </pre>
+  *
+  * Unlike traditional repeated parameters,
+  * which restrict the sequence argument at the last position,
+  * sequence arguments in a curried call are allowed at any position.
+  *
+  * When a [[Curried]] is invoked with some type arguments,
+  * those type arguments will be moved to the `applyBegin` method.
+  * Therefore, `List[Int](1 to 3: _*)` should be translated to `(1 to 3).foldLeft(List.applyBegin[Int])(_.applyNext(_)).applyEnd`.
+  *
+  * @example Fast list builder
+  *
+  * {{{
+  * class PartiallyAppliedInitializer[A](builder: collection.mutable.Builder[A, List[A]]) {
+  *   def applyEnd = builder.result
+  *   def applyNext(a: A) = {
+  *     builder += a
+  *     this
+  *   }
+  * }
+  * object FastListInitializer extends Curried {
+  *   def applyBegin[A]:PartiallyAppliedInitializer[A] = new PartiallyAppliedInitializer(List.newBuilder[A])
+  * }
+  *
+  * FastListInitializer(0, 4) should be(List(0, 4))
+  * FastListInitializer[Int](0, 100 to 103: _*, 1) should be(List(0, 100, 101, 102, 103, 1))
+  * }}}
+  */
+trait Curried extends Any with CurriedWithTypeParameters {
+  def apply(varargs: Any*): Any = macro Curried.Macros.apply
+}
